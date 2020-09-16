afe5f97f554bf4c9476db2143f8b60dc764cd07e
Christofer Karankevits <chris.karankevits@gmail.com>
2020-09-16 11:00:35 +0300

    Создали и вывили массив 20 целых чисел

--- src/sptvr19myarray/App.java
+++ src/sptvr19myarray/App.java
@@ -5,6 +5,7 @@
  */
 package sptvr19myarray;
 
+import java.util.Random;
 /**
  *
  * @author user
@@ -12,5 +13,13 @@ package sptvr19myarray;
 class App {
     public void run() {
         System.out.println("--Array--");
+        Random random = new Random();
+        //Содаём массив целых чётных чисел
+        int[] numbers = new int[20];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(99-0+1)+0;
+            System.out.printf("%4d",numbers[i]);
+        }
+        System.out.println();
     }
 }
